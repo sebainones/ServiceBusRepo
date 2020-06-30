6fc7008b976d8bec18a0131daf042282574ee497
Sebastián Iñones <sebainones@gmail.com>
2020-06-30 08:53:08 +0200

    Initial commit

---
+++ ServiceBus/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace ServiceBus
+{
+    class Program
+    {
+        private static string ServiceBusConnectionString;
+        private static string QueueName;
+        static IQueueClient queueClient;
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                IConfigurationBuilder configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot config = configBuilder.Build();
+
+                ServiceBusConnectionString = config["ServiceBusConnectionString"];
+                QueueName = config["QueueName"];
+
+                const int numberOfMessages = 10;
+
+                queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+                // Register QueueClient's MessageHandler and receive messages in a loop
+                RegisterOnMessageHandlerAndReceiveMessages();
+
+                // Send messages.
+                await SendMessagesAsync(numberOfMessages);
+
+                Console.WriteLine("Now, we will check some incoming messages");
+
+
+                Thread.Sleep(5000);
+                Console.WriteLine("Now... closing QUEUE");
+                await queueClient.CloseAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the message handler options in terms of exception handling, number of concurrent messages to deliver, etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of concurrent calls to the callback ProcessMessagesAsync(), set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether the message pump should automatically complete the messages after returning from user callback.
+                // False below indicates the complete operation is handled by the user callback as in ProcessMessagesAsync().
+                AutoComplete = false
+            };
+
+            // Register the function that processes messages.
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message.
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queue Client is created in ReceiveMode.PeekLock mode (which is the default).
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been closed, you can choose to not call CompleteAsync() or AbandonAsync() etc.
+            // to avoid unnecessary exceptions.
+        }
+
+        // Use this handler to examine the exceptions received on the message pump.
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+
+        private static async Task SendMessagesAsync(int numberOfMessages)
+        {
+            try
+            {
+                for (var i = 0; i < numberOfMessages; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message number {i}";
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    // Write the body of the message to the console.
+                    Console.WriteLine($"Sending message: {messageBody}");
+
+                    // Send the message to the queue.
+                    await queueClient.SendAsync(message);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+        }
+    }
+}
