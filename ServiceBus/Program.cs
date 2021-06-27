aff5e915af4c56f26db6a9830fe201a025896f7a
sebainones <seba.inones@gmail.com>
2021-06-27 10:29:12 +0200

    Only Queue logic

--- ServiceBus/Program.cs
+++ ServiceBus/Program.cs
@@ -9,23 +9,17 @@ namespace ServiceBus
 {
     class Program
     {
-        static IQueueClient queueClient; //To send messages to a QUEUE
+        //To send messages to a QUEUE
+
+        private static IQueueClient queueClient;
+        private static IQueueClient QueueClient => queueClient ??= QueueHandler.CreateQueClient(Configuration.ServiceBusConnectionString, Configuration.QueueName);
 
         public static async Task Main(string[] args)
         {
             try
             {
-
-                //Now we us
-                queueClient = QueueHandler.CreateQueClient(Configuration.ServiceBusConnectionString, Configuration.QueueName);
-
-                //topicClient = TopicHandler.CreatTopicClient(serviceBusSecretConnectionString.Value, Configuration.TopicName);
-
                 // Register QueueClient's MessageHandler and receive messages.
                 RegisterOnMessageHandlerAndReceiveMessages();
-
-                //To receive messages, you must create a SubscriptionClient object, NOT  a TopicClient object
-                //SubscribeToReeiveTopicMessages(serviceBusSecretConnectionString.Value);
 
                 // Send messages.
                 await SendQueueMessagesAsync();
@@ -41,32 +35,10 @@ namespace ServiceBus
             finally
             {
                 Console.WriteLine("Now... closing QUEUE");
-                await queueClient.CloseAsync();
+                if (QueueClient != null)
+                    await QueueClient.CloseAsync();
+
             }
-        }
-
-        private static void SubscribeToReeiveTopicMessages(string serviceBusConnectionString)
-        {
-            subscriptionClient = new SubscriptionClient(serviceBusConnectionString, Configuration.TopicName, Configuration.SubscriptionName);
-
-            //TODO: learn how to properly add FILTERS!!!
-            //subscriptionClient.AddRuleAsync(new RuleDescription { Filter = new SqlFilter("From LIKE '%Smith'") });
-
-            // Configure the message handler options in terms of exception handling, number of concurrent messages to deliver, etc.
-            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
-            {
-                // Maximum number of concurrent calls to the callback ProcessMessagesAsync(), set to 1 for simplicity.
-                // Set it according to how many messages the application wants to process in parallel.
-                MaxConcurrentCalls = 1,
-
-                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
-                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
-                AutoComplete = false
-            };
-
-            // Register the function that processes messages.
-
-            subscriptionClient.RegisterMessageHandler(ProcessTopicMessagesAsync, messageHandlerOptions);
         }
 
         private static void RegisterOnMessageHandlerAndReceiveMessages()
@@ -84,7 +56,7 @@ namespace ServiceBus
             };
 
             // Register the function that processes messages.
-            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+            QueueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
         }
 
         private static async Task ProcessMessagesAsync(Message message, CancellationToken token)
@@ -94,7 +66,7 @@ namespace ServiceBus
 
             // Complete the message so that it is not received again.
             // This can be done only if the queue Client is created in ReceiveMode.PeekLock mode (which is the default).
-            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+            await QueueClient.CompleteAsync(message.SystemProperties.LockToken);
 
             // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
             // If queueClient has already been closed, you can choose to not call CompleteAsync() or AbandonAsync() etc.
@@ -130,7 +102,7 @@ namespace ServiceBus
                     Console.WriteLine($"Sending message: {messageBody}");
 
                     // Send the message to the queue.
-                    await queueClient.SendAsync(encodedMessage);
+                    await QueueClient.SendAsync(encodedMessage);
                 }
             }
             catch (Exception exception)
